fc47881a81a986dc4210d42d0f6c01aefaa20ddc
kanishka sahu <kanishkasahu005@gmail.com>
2025-06-21 12:07:13 +0530

    auth middleware added

---
+++ backend/app/middleware/auth_middleware.py
@@ -0,0 +1,34 @@
+from fastapi import HTTPException, Depends
+from jose import jwt
+from app.models.user import User
+from app.db.database import get_db
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi.security import OAuth2PasswordBearer
+import os
+from app.schemas.user import UserResponse
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+async def auth_middleware(
+    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
+):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+
+        user_query = await db.execute(select(User).where(User.id == payload["id"]))
+
+        user = user_query.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        if not user.is_active:
+            raise HTTPException(status_code=401, detail="User is not active")
+        data = UserResponse(**user.__dict__)
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=401, detail=str(e))
